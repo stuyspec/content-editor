ed6236f31fee47bef5f258185e3028e0265c635f
Nicholas Yang <ny585@nyu.edu>
2017-11-04 18:15:19 -0400

    made into component library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+import ContentEditor from './ContentEditor'
+
+module.exports = {
+  ContentEditor
+};
