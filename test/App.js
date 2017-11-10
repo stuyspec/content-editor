94960292d41b6848e4bf18140f84fd9b4d91206c
Nicholas Yang <ny585@nyu.edu>
2017-11-10 11:46:01 -0500

    modified the image interface

--- test/App.js
+++ test/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { EditorState } from "draft-js";
 import ContentEditor from "../lib/ContentEditor";
 
 const data = {
   article_id: 1,
   user_id: 1
-}
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: EditorState.createEmpty()
+      editorState: EditorState.createEmpty(),
+      images: []
     };
+    this.fetchImages();
   }
 
   handleChange = editorState => {
     this.setState({ editorState });
+  };
+
+  addImageToState = image => {
+    const { images } = this.state;
+    this.setState({ images: [...images, image] });
+  };
+
+  fetchImages = () => {
+    axios.get("http://localhost:3000/media").then(response => {
+      this.setState({ images: response.data });
+    });
   };
 
   render() {
@@ -24,6 +39,7 @@ class App extends Component {
         <ContentEditor
           uploadData={data}
           onChange={this.handleChange}
+          images={this.state.images}
           editorState={this.state.editorState}
         />
       </div>
